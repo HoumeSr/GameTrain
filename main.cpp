ba86426b8ecdde0c5df2184bc5ccd66a50f89cc1
Камиль Гимранов <gkr004@campus.mephi.ru>
2024-06-22 21:29:28 +0300

    Object class, linking to time

--- main.cpp
+++ main.cpp
@@ -1,18 +1,44 @@
 #include <SFML/Graphics.hpp>
+#include "iostream"
 using namespace std;
 using namespace sf;
+
+class object{
+private:
+    double x_;
+    double y_;
+//    double w_;
+//    double h_;
+    Image image_;
+    Texture texture_;
+    Sprite sprite_;
+public:
+    object(double x, double y/*, double w, double h*/, string FILEPath){
+        image_.loadFromFile(FILEPath);
+        texture_.loadFromImage(image_);
+        sprite_.setTexture(texture_);
+        sprite_.setPosition(x, y);
+        x_ = x;
+        y_ = y;
+//        w_ = w;
+//        h_ = h;
+        //Also we can change type of sprite(texture) based on type of img;
+    }
+    void update(double speed, double time, vector<int> direction){ // direction[0] - 0/1/-1 - direction of opbject by Ox?. Same for direction[1];
+        x_ += speed * time * direction[0];
+        y_ += speed * time * direction[1];
+        sprite_.setPosition(x_, y_);
+    }
+    Sprite GetSprite(){
+        return sprite_;
+    }
+};
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Catch the Hilter");
-    Image ilnur_img;
-    ilnur_img.loadFromFile("images/adolf.jpg");
-    Texture ilnur_txt, stalin_txt;
-    ilnur_txt.loadFromImage(ilnur_img);
-    stalin_txt.loadFromFile("images/stalin.jpg");
-    Sprite ilnur, stalin;
-    stalin.setTexture(stalin_txt);
-    ilnur.setTexture(ilnur_txt);
+    object ilnur(0, 0, "images/ilnur.jpg");
+    Clock clock;
     double speed = 0.5;
     while (window.isOpen())
     {
@@ -22,36 +48,24 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        double time = clock.restart().asMicroseconds();
+        time /= 1000;
+        cout << time << '\n';
+        vector<int> direction(2, 0);
         if(Keyboard::isKeyPressed(Keyboard::W)){
-            ilnur.move(0, -speed);
+            ilnur.update(speed, time, {0, -1});
         }
         if(Keyboard::isKeyPressed(Keyboard::A)){
-            ilnur.move(-speed, 0);
+            ilnur.update(speed, time, {-1, 0});
         }
         if(Keyboard::isKeyPressed(Keyboard::S)){
-            ilnur.move(0, speed);
+            ilnur.update(speed, time, {0, 1});
         }
         if(Keyboard::isKeyPressed(Keyboard::D)){
-            ilnur.move(speed, 0);
+            ilnur.update(speed, time, {1, 0});
         }
-
-        if(Keyboard::isKeyPressed(Keyboard::Up)){
-            stalin.move(0, -speed);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Left)){
-            stalin.move(-speed, 0);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Down)){
-            stalin.move(0, speed);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Right)){
-            stalin.move(speed, 0);
-        }
-
-
         window.clear();
-        window.draw(ilnur);
-        window.draw(stalin);
+        window.draw(ilnur.GetSprite());
         window.display();
     }
 
