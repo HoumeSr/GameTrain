2bdcd19f0de663147482a0bf2945296166d350d6
Houmedis <ilnurik04@mail.ru>
2024-06-22 17:20:42 +0300

    Сделал кружок зелёным

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
