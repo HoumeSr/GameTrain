4d1fcc7aaf7c9e5ed589e35100553abcde8869ea
Камиль Гимранов <gkr004@campus.mephi.ru>
2024-06-22 18:35:22 +0300

    Historical update (v. 1.1)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Catch the Hilter");
     Image ilnur_img;
-    ilnur_img.loadFromFile("../images/ilnur.jpg");
-    Texture ilnur_txt;
+    ilnur_img.loadFromFile("images/adolf.jpg");
+    Texture ilnur_txt, stalin_txt;
     ilnur_txt.loadFromImage(ilnur_img);
-    Sprite ilnur;
+    stalin_txt.loadFromFile("images/stalin.jpg");
+    Sprite ilnur, stalin;
+    stalin.setTexture(stalin_txt);
     ilnur.setTexture(ilnur_txt);
     double speed = 0.5;
     while (window.isOpen())
@@ -32,9 +35,23 @@ int main()
             ilnur.move(speed, 0);
         }
 
+        if(Keyboard::isKeyPressed(Keyboard::Up)){
+            stalin.move(0, -speed);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Left)){
+            stalin.move(-speed, 0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Down)){
+            stalin.move(0, speed);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Right)){
+            stalin.move(speed, 0);
+        }
+
 
         window.clear();
         window.draw(ilnur);
+        window.draw(stalin);
         window.display();
     }
 
