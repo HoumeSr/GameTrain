943cbf723de464c569783631774356e668cf7494
Камиль Гимранов <gkr004@campus.mephi.ru>
2024-06-30 10:43:02 +0300

    Теперь за карту не выходит, но нужно спрайту координаты не в левый верхний угол а в центр сделать чтобы не норм выглядел выходи за карту

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ class object{
 private:
     double x_;
     double y_;
-//    double w_;
-//    double h_;
     Image image_;
     Texture texture_;
     Sprite sprite_;
+    bool synergy_with_map_rect(vector<vector<int>> rectangles, double speed, double time, vector<int> direction) { // true - right, false - left(unblocked side)
+        double x = x_ + speed * time * direction[0];
+        double y = y_ + speed * time * direction[1];
+        for(size_t i = 0; i < rectangles.size(); ++i) {
+            double left = rectangles[i][0], right = rectangles[i][1];
+            double lower = rectangles[i][2], upper = rectangles[i][3];
+            if (left <= x && x <= right) {
+                if (!(lower <= y && y <= upper)) {
+                    return false;
+                }
+            }
+            else return false;
+        }
+        return true;
+    }
 public:
     object(double x, double y/*, double w, double h*/, string FILEPath){
         image_.loadFromFile(FILEPath);
@@ -22,13 +35,27 @@ public:
         y_ = y;
         //Also we can change type of sprite(texture) based on type of img;
     }
-    void update(double speed, double time, vector<int> direction){ // direction[0] - 0/1/-1 - direction of opbject by Ox?. Same for direction[1];
+    void update(double speed, double time, vector<int> direction, const vector<vector<int>>& rectangles){ // direction[0] - 0/1/-1 - direction of opbject by Ox?. Same for direction[1];
+        if(!synergy_with_map_rect(rectangles, speed, time, direction)) return;
         x_ += speed * time * direction[0];
         y_ += speed * time * direction[1];
         sprite_.setPosition(x_, y_);
     }
+    void update(double speed, double time, vector<int> direction, const vector<vector<int>>& rectangles, View& view){ // overloading with pinned view
+        if(!synergy_with_map_rect(rectangles, speed, time, direction)) return;
+        x_ += speed * time * direction[0];
+        y_ += speed * time * direction[1];
+        sprite_.setPosition(x_, y_);
+        view.move(speed * time * direction[0], speed * time * direction[1]);
+    }
     Sprite GetSprite(){
         return sprite_;
+    }
+    double get_x(){
+        return x_;
+    }
+    double get_y(){
+        return y_;
     }
 };
 
@@ -36,15 +63,14 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "Catch the Hilter");
     sf::RenderWindow window_clone(sf::VideoMode(1000, 1000), "Catch the Hilter 2 player");
-    object ilnur(0, 0, "images/ilnur.jpg");
-    object cat(0, 0, "images/cat_s.png");
-    object mouse(0, 0, "images/mouse_s.png");
+
+
     Clock clock;
     double speed = 0.5;
     View view, view_clone;
+    //map's sprite's rendering
     int map_w = 200, map_h = 200,  tile_w = 64, tile_h = 64;
     vector<string> map = createMap(map_w, map_h);
-
     sf::RenderTexture renderTexture;
     renderTexture.create(map_w * tile_w, map_h * tile_w);
     Texture map_wall, map_oth;
@@ -76,11 +102,14 @@ int main()
             }
         }
     }
-
+    vector<int> rectangle1 = {0 + tile_w, map_w * tile_w - tile_w, 0 + tile_h, map_h * tile_h - tile_h};
     renderTexture.display();
-
     sf::Sprite mapSprite;
     mapSprite.setTexture(renderTexture.getTexture());
+
+    object ilnur(tile_w, tile_h, "images/ilnur.jpg");
+    object cat(2 * tile_w, 2 * tile_h, "images/cat_s.png");
+    object mouse(tile_w, tile_h, "images/mouse_s.png");
 
     while (window.isOpen() || window_clone.isOpen())
     {
@@ -97,39 +126,32 @@ int main()
         }
         double time = clock.restart().asMicroseconds();
         time /= 1000;
-        cout << time << '\n';
+//        cout << time << '\n';
         if(Keyboard::isKeyPressed(Keyboard::W)){
-            cat.update(speed, time, {0, -1});
-            view.move(0, speed * time * -1);
+            cat.update(speed, time, {0, -1}, {rectangle1}, view);
         }
         if(Keyboard::isKeyPressed(Keyboard::A)){
-            cat.update(speed, time, {-1, 0});
-            view.move(speed * time * -1, 0);
+            cat.update(speed, time, {-1, 0}, {rectangle1}, view);
         }
         if(Keyboard::isKeyPressed(Keyboard::S)){
-            cat.update(speed, time, {0, 1});
-            view.move(0, speed * time * 1);
+            cat.update(speed, time, {0, 1}, {rectangle1}, view);
         }
         if(Keyboard::isKeyPressed(Keyboard::D)){
-            cat.update(speed, time, {1, 0});
-            view.move(speed * time * 1, 0);
+            cat.update(speed, time, {1, 0}, {rectangle1}, view);
         }
         if(Keyboard::isKeyPressed(Keyboard::Up)){
-            mouse.update(speed, time, {0, -1});
-            view_clone.move(0, speed * time * -1);
+            mouse.update(speed, time, {0, -1}, {rectangle1}, view_clone);
         }
         if(Keyboard::isKeyPressed(Keyboard::Left)){
-            mouse.update(speed, time, {-1, 0});
-            view_clone.move(speed * time * -1, 0);
+            mouse.update(speed, time, {-1, 0}, {rectangle1}, view_clone);
         }
         if(Keyboard::isKeyPressed(Keyboard::Down)){
-            mouse.update(speed, time, {0, 1});
-            view_clone.move(0, speed * time * 1);
+            mouse.update(speed, time, {0, 1}, {rectangle1}, view_clone);
         }
         if(Keyboard::isKeyPressed(Keyboard::Right)){
-            mouse.update(speed, time, {1, 0});
-            view_clone.move(speed * time * 1, 0);
+            mouse.update(speed, time, {1, 0}, {rectangle1}, view_clone);
         }
+        //drawing
         window.setView(view);
         window.clear();
         window.draw(mapSprite);
